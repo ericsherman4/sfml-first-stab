7fe82c3b8ee70ad3702a69424e4ed556a39aa939
ericsherman4 <esherman2018@gmail.com>
2023-11-11 12:05:55 -0500

    keyboard movement

--- first-game/Sim.cpp
+++ first-game/Sim.cpp
@@ -3,11 +3,15 @@
 
 Sim::Sim()
     : window(sf::VideoMode(1920, 1080), "First Game!"),
-    player()
+    player(),
+    pos(INIT_X, INIT_Y),
+    vel(-10,3)
 {
-    player.setRadius(100.f);
-    player.setPosition(500.f, 500.f);
+    player.setRadius(20.f);
+    player.setPosition(pos);
     player.setFillColor(sf::Color::Magenta);
+
+    isMovingUp = isMovingDown = isMovingRight = isMovingLeft = false;
 }
 
 void Sim::Run()
@@ -20,6 +24,27 @@ void Sim::Run()
     }
 }
 
+void Sim::HandlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+
+    switch (key)
+    {
+        case sf::Keyboard::Key::W:
+            isMovingUp = isPressed;
+            break;
+        case sf::Keyboard::Key::A:
+            isMovingLeft = isPressed;
+            break;
+        case sf::Keyboard::Key::S:
+            isMovingDown = isPressed;
+            break;
+        case sf::Keyboard::Key::D:
+            isMovingRight = isPressed;
+            break;
+    }
+
+}
+
 void Sim::ProcessEvents()
 {
     if (window.isOpen())
@@ -27,6 +52,18 @@ void Sim::ProcessEvents()
         sf::Event event;
         while (window.pollEvent(event))
         {
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    HandlePlayerInput(event.key.code, true);
+                    break;
+                case sf::Event::KeyReleased:
+                    HandlePlayerInput(event.key.code, false);
+                    break;
+            }
             if (event.type == sf::Event::Closed)
                 window.close();
         }
@@ -35,6 +72,24 @@ void Sim::ProcessEvents()
 
 void Sim::UpdateModel()
 {
+    sf::Vector2f movement(0.f, 0.f);
+    if (isMovingDown)
+    {
+        movement.y += 0.5f;
+    }
+    if (isMovingRight)
+    {
+        movement.x += 0.5f;
+    }
+    if (isMovingLeft)
+    {
+        movement.x -= 0.5f;
+    }
+    if (isMovingUp)
+    {
+        movement.y -= 0.5f;
+    }
+    player.move(movement);
 
 }
 
--- first-game/Sim.h
+++ first-game/Sim.h
@@ -4,7 +4,7 @@
 class Sim
 {
 private:
-    
+    void HandlePlayerInput(sf::Keyboard::Key key, bool isPressed);
     void ProcessEvents();
     void UpdateModel();
     void Display();
@@ -12,7 +12,17 @@ public:
     Sim();
     void Run();
 private:
+
+    static constexpr float INIT_X = 500.f;
+    static constexpr float INIT_Y = 500.f;
+
     sf::RenderWindow window; 
     sf::CircleShape player;
+    bool isMovingUp;
+    bool isMovingDown;
+    bool isMovingRight;
+    bool isMovingLeft;
+    sf::Vector2f pos;
+    sf::Vector2f vel;
 };
 
